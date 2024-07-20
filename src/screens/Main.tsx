78bf99bdfd50e72cd546e8a166aee1a3d9aed0df
Minyoung <alsendrha@naver.com>
2024-07-20 16:43:18 +0900

    first commit

---
+++ src/screens/Main.tsx
@@ -0,0 +1,14 @@
+import {Text, View} from 'react-native';
+import React from 'react';
+
+const Main = () => {
+  return (
+    <View>
+      <View className="w-full h-[200px] border">
+        <Text className="text-white text-lg">아아</Text>
+      </View>
+    </View>
+  );
+};
+
+export default Main;
